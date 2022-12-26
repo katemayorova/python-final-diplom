f31dbb3fcad4bfa4a0c193f8e49ec852a1a66261
katemayorova <katemayorova0@gmail.com>
2022-12-26 22:27:51 +0300

    Add AUTH USER MODEL

--- orders/settings.py
+++ orders/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'backend.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
