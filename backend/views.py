78a998f11cee49984e89ccc5cb54d718ffd8e329
katemayorova <katemayorova0@gmail.com>
2022-12-27 21:34:43 +0300

    Add Views for Category, Shop

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from serializers import UserSerializer, OrderSerializer, CategorySerializer, ShopSerializer, \
+    ProductInfoSerializer, OrderItemSerializer, ContactSerializer
+from api.models import Order, OrderItem, Product, ConfirmEmailToken, Category, Shop, \
+    ProductInfo, Parameter, ProductParameter, Contact
 
-# Create your views here.
+
+class CategoryView(ListAPIView):
+    """
+    Класс для просмотра категорий
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ShopView(ListAPIView):
+    """
+    Класс для просмотра списка магазинов
+    """
+    queryset = Shop.objects.filter(state=True)
+    serializer_class = ShopSerializer
